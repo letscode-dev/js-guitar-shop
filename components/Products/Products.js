db8621c9aa09980de8e2a3dd84a6d838861179f5
anton-sergeenkov <warnes23@yandex.ru>
2020-01-07 19:37:17 +0300

    starter

--- components/Products/Products.js
+++ components/Products/Products.js
@@ -1,53 +1,17 @@
-// import { ROOT_PRODUCTS }    from '/constants/root.js';
-// import { CATALOG }          from '/constants/catalog.js';
-// import { localStorageUtil } from 'utils/localStorageUtil.js';
-
 class Products {
-    constructor() {
-        this.classNameActive = 'products-element__btn_active';
-        this.labelAdd = 'Добавить в корзину';
-        this.labelRemove = 'Удалить из корзины';
-    }
-
-    handlerSetLocatStorage(element, id) {
-        const result = localStorageUtil.putProducts(id);
-
-        if (result.pushProduct) {
-            element.classList.add(this.classNameActive);
-            element.innerText = this.labelRemove;
-        } else {
-            element.classList.remove(this.classNameActive);
-            element.innerText = this.labelAdd;
-        }
-
-        headerPage.render(result.products.length);
-    }
 
     render() {
-        const productsStore = localStorageUtil.getProducts();
         let htmlCatalog = '';
 
         CATALOG.forEach(({ id, name, price, img }) => {
-            let activeClass = '';
-            let activeText = '';
-
-            if (productsStore.indexOf(id) === -1) {
-                activeText = this.labelAdd;
-            } else {
-                activeClass = ' ' + this.classNameActive;
-                activeText = this.labelRemove;
-            }
-
             htmlCatalog += `
                 <li class="products-element">
-                    <div class="products-element__name">${name}</div>
+                    <span class="products-element__name">${name}</span>
                     <img class="products-element__img" src="${img}" />
                     <span class="products-element__price">
                         ⚡️ ${price.toLocaleString()} USD
                     </span>
-                    <button class="products-element__btn${activeClass}" onclick="productsPage.handlerSetLocatStorage(this, '${id}');">
-                        ${activeText}
-                    </button>
+                    <button class="products-element__btn">Добавить в корзину</button>
                 </li>
             `;
         });
@@ -60,7 +24,7 @@ class Products {
 
         ROOT_PRODUCTS.innerHTML = html;
     }
-};
+}
 
 const productsPage = new Products();
 productsPage.render();
