6eb01cb4314324a432b22743f25f26327f0fbc87
anton-sergeenkov <warnes23@yandex.ru>
2020-01-25 22:09:09 +0300

    starter

--- components/Header/Header.js
+++ components/Header/Header.js
@@ -1,24 +1,16 @@
-// import { ROOT_HEADER }      from '/constants/root.js';
-// import { shoppingPage }     from 'components/Shopping/Shopping.js';
-// import { localStorageUtil } from 'utils/localStorageUtil.js';
-
 class Header {
-    handlerOpenShoppingPage() {
-        shoppingPage.render();
-    }
-
     render(count) {
         const html = `
-            <div class="header-container">
-                <div class="header-counter" onclick="headerPage.handlerOpenShoppingPage();">
+           <div class="header-container">
+                <div class="header-counter">
                     🔥 ${count}
                 </div>
-            </div>
+           </div>
         `;
 
         ROOT_HEADER.innerHTML = html;
     }
-};
+}
 
 const headerPage = new Header();
 
